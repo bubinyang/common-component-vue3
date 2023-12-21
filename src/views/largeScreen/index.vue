860ad1348e1e91cbab8714fc455c36e3d2eded99
bubinyang12 <446490517@qq.com>
2023-12-21 20:58:46 +0800

    update

--- src/views/largeScreen/index.vue
+++ src/views/largeScreen/index.vue
@@ -30,7 +30,7 @@
       <section class="screen-main-content">
         <section class="screen-main-l">
           <SmallContain :title="'设备状态'">
-            <twoNew></twoNew>
+            <twoNew v-if="refresAllDayPoint"></twoNew>
             <!-- <two v-if="refresAllDayPoint"></two> -->
           </SmallContain>
 
@@ -104,7 +104,7 @@ export default {
     const updateData = () => {
       this.refreshComponent();
       this.init();
-      setTimeout(updateData, 300000);
+      setTimeout(updateData, 10000);
     };
     updateData();
 
